3ceb61fa1ebe96fa165792108ecaea558f99a4f8
Amelie Yeh <amelieyeh@gmail.com>
2017-07-05 18:37:05 +0800

    Redux reducer composition

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,7 +7,7 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     )
@@ -15,13 +15,14 @@ const Comments = React.createClass({
 
   handleSubmit(e){
     e.preventDefault();
-    console.log("Submitting!!!!");
+    // console.log("Submitting!!!!");
     const { postId } = this.props.params;
-    console.log(this.refs);
+    // console.log(this.refs);
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
-    console.log(postId, author, comment);
+    // console.log(postId, author, comment);
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
 
   render() {
